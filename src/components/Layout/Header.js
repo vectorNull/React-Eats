1c26d393d3de9b67233b772cd2b8d04c6a740b26
Jose Rodriguez <jose.rodriguez@prominentcode.com>
2022-05-01 23:47:24 -0700

    build cart component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,20 +1,21 @@
-import { Fragment } from 'react';
-import classes from './Header.module.css';
+import { Fragment } from "react";
+import HeaderCartButton from "./HeaderCartButton";
+import classes from "./Header.module.css";
 
-import mealsImg from '../../Assets/meals.jpg'
+import mealsImg from "../../Assets/meals.jpg";
 
 const Header = (props) => {
-  return (
-    <Fragment>
-        <header className={classes.header}>
-            <h1>React Eats</h1>
-            <button>Cart</button>
-        </header>
-        <div className={classes['main-image']}>
-            <img src={mealsImg} alt="food on table" />
-        </div>
-    </Fragment>
-  )
-}
+    return (
+        <Fragment>
+            <header className={classes.header}>
+                <h1>React Eats</h1>
+                <HeaderCartButton />
+            </header>
+            <div className={classes["main-image"]}>
+                <img src={mealsImg} alt="food on table" />
+            </div>
+        </Fragment>
+    );
+};
 
-export default Header
+export default Header;
