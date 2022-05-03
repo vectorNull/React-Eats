0eb62f7cfa6816fea99a1f939f5088f0c4f49c29
Jose Rodriguez <jose.rodriguez@prominentcode.com>
2022-05-02 20:56:58 -0700

    build shopping cart components

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,31 @@
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
+import classes from "./Modal.module.css";
+
+const Backdrop = (props) => {
+	return <div className={classes.backdrop}></div>;
+};
+
+const ModalOverlay = (props) => {
+	return (
+		<div className={classes.modal}>
+			<div classname={classes.content}>{props.children}</div>
+		</div>
+	);
+};
+
+const Modal = (props) => {
+	const portalElement = document.getElementById("overlays");
+
+	return (
+		<Fragment>
+			{ReactDOM.createPortal(<Backdrop />, portalElement)}
+			{ReactDOM.createPortal(
+				<ModalOverlay>{props.children}</ModalOverlay>,
+				portalElement
+			)}
+		</Fragment>
+	);
+};
+
+export default Modal;
